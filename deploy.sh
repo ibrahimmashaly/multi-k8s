261e83a6da99b08b3f3160be256bfb1efa261e0d
ibrahimmashaly <ibrahimmashaly@hotmail.com>
2019-10-05 14:34:15 +0300

    capital K not small

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker push imashaly/multi-worker:latest
 docker push imashaly/multi-client:$SHA
 docker push imashaly/multi-server:$SHA
 docker push imashaly/multi-worker:$SHA
-kubectl apply -f k8s
+kubectl apply -f K8s
 kubectl set image deployments/server-deployment server=imashaly/multi-server:$SHA
 kubectl set image deployments/client-deployment client=imashaly/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=imashaly/multi-worker:$SHA
